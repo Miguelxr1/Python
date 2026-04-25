054f59cde8b7f92b17bb9a2cca62dceea088bf74
Miguelxrs1 <miguelmpacheco8218@gmail.com>
2026-04-24 23:06:07 -0300

    Peguei a resposta do des84 com o Claude e não conferi nem testei pois estou com muita prequiça agora

--- codes/CursoemVideo/Aulas/aula18.py
+++ codes/CursoemVideo/Aulas/aula18.py
@@ -1,9 +1,28 @@
 teste = list()
 teste.append('Miguel')
 teste.append(14)
-galera =list()
-galera.append(teste[:])
+galera1 =list()
+galera1.append(teste[:])
 teste[0] = 'Maria'
 teste[1] = 22
-galera.append(teste[:])
-print(galera)
+galera1.append(teste[:])
+print(galera1)
+
+galera2 = [['João', 19], ['Ana', 33], ['Joaquim', 13], ['Maria', 45]]
+print(galera2)
+#Se eu mandar somente [0], ele printará a primeira lista, mas se eu mandar [0][0], ele printará o primeiro elemento da primeira lista. Veja o exemplo:
+print(galera2[0]) # Printa a primeira lista dentro da lista
+print(galera2[0][0]) # Printa o primeiro item da primeira lista dentro da lista
+for p in galera2:
+    print(p)
+    
+
+galera3 = list()
+dados = list()
+
+for c in range(0, 3):
+    dados.append(str(input('Nome: ')))
+    dados.append(int(input('Idade: ')))
+    galera3.append(dados[:])
+    dados.clear()
+print(galera3)
---
+++ codes/CursoemVideo/Desafios/des84.py
@@ -0,0 +1,33 @@
+pessoas = list()
+dados = list()
+c = 0
+
+while True:
+    c += 1
+    dados.append(str(input('Nome: ')))
+    dados.append(int(input('Peso: ')))
+    pessoas.append(dados[:])
+    dados.clear()
+    p = input('Deseja continuar? [S/N] ').upper().strip()
+    if p == 'N':
+        break
+
+
+maior_peso = pessoas[0][1]
+maior_nome = pessoas[0][0]   
+menor_peso = pessoas[0][1]
+menor_nome = pessoas[0][0]
+
+for pessoa in pessoas:
+    nome = pessoa[0] 
+    peso = pessoa[1] 
+    if peso > maior_peso:
+        maior_peso = peso
+        maior_nome = nome
+    if peso < menor_peso:
+        menor_peso = peso
+        menor_nome = nome
+
+print(f'Foram cadastradas {c} pessoas.')
+print(f'O maior peso foi de {maior_peso}Kg. Peso de {maior_nome}')
+print(f'O menor peso foi de {menor_peso}Kg. Peso de {menor_nome}')
