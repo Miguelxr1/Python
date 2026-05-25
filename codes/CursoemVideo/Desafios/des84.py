305508a3b1767e1f7cd511a2279f026ee47f6404
Miguelxrs1 <miguelmpacheco8218@gmail.com>
2026-05-24 22:05:17 -0300

    Corrigido até o desafio 85

--- codes/CursoemVideo/Desafios/des84.py
+++ codes/CursoemVideo/Desafios/des84.py
@@ -1,35 +1,31 @@
 temp = list()
 princ = list()
-mai = men = 0
+maior = menor = 0
 
 while True:
     temp.append(str(input('Nome: ')))
     temp.append(float(input('Peso: ')))
+    
     if len(princ) == 0:
-        mai = men = temp[1]
+        maior = menor = temp[1]
     else:
-        if temp[1] > mai:
-            mai = temp[1]
-        if temp[1] < men:
-            men = temp[1]
+        if temp[1] > maior:
+            maior = temp[1]
+        if temp[1] < menor:
+            menor = temp[1]
+    
     princ.append(temp[:])
-    temp.clear()
     resp = str(input('Quer continuar? [S/N] '))
+    temp.clear()
     if resp in 'Nn':
         break
-print('-' * 30)
-
-print(f'Os dados foram: {princ}')
-print(f'Foram cadastradas {len(princ)} pessoas.')
-print(f'O menor peso foi de {mai}Kg. Peso de', end=' ')
-
+print('-----'*20)
+print(f'Você cadastrou {len(princ)} pessoas.')
+print(f'O maior peso foi de {maior}Kg', end=' ')
 for p in princ:
-    if p[1] == mai:
-        print(f'[{p[0]}]', end='')
-print()
-
-print(f'O menor peso foi de {men}Kg. Peso de', end=' ')
-for i in princ:
-    if p[1] == men:
-        print(f'[{p[0]}]', end=' ')
-print()
+    if p[1] == maior:
+        print(p[0], end=' ')
+print(f'\nO menor peso foi de {menor}Kg', end=' ')
+for p in princ:
+    if p[1] == menor:
+        print(p[0], end=' ')
--- codes/CursoemVideo/Desafios/des85.py
+++ codes/CursoemVideo/Desafios/des85.py
@@ -1,16 +1,14 @@
-n = list()
-par = list()
-impar = list()
+n = [[], []]
+v =0
 
 for i in range(0, 7):
-    p = int(input("Digite um número: "))
-    if p % 2 == 0:
-        par.append(p)
-    elif p % 2 != 0:
-        impar.append(p)
-par.sort()
-impar.sort()
-n.append(par[:])
-n.append(impar[:])
-print(f'\nOs valores Pares são: {n[0]}')
+    v = int(input("Digite um número: "))
+    if v % 2 == 0:
+        n[0].append(v)
+    elif v % 2 != 0:
+        n[1].append(v)
+n[0].sort()
+n[1].sort()
+print('-----'*20)
+print(f'Os valores Pares são: {n[0]}')
 print(f'Os valores impares são: {n[1]}')
