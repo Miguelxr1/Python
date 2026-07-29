55612abf7162192e2c677a9cf99a882694e6fd1e
Miguel M. Pacheco <miguelmpacheco@email.com>
2026-07-28 22:27:49 -0300

    Feito até parte do desafio 105

---
+++ codes/CursoemVideo/Desafios/des105.py
@@ -0,0 +1,29 @@
+dados = {'total': 0, 'maior': 0, 'menor': 0, 'média': 0}
+
+def notas(*num, sit=False):
+    dados['total'] = len(num)
+    
+    maior = num[0]
+    for n in num:
+        if n > maior:
+            maior = n
+    dados['maior'] = maior
+    
+    menor = num[0]
+    for n in num:
+        if n < menor:
+            menor = n
+    dados['menor'] = menor
+    
+    s = 0
+    for n in num:
+        s += n
+    dados['média'] = s/len(num)
+    
+    if sit:
+        if dados['média'] < 5:
+            dados['média'] = 'RUIM'    
+    return dados
+
+resp = notas(5.5, 9.5, 10, 6.5)
+print(resp)
