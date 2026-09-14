30b690477b7d1e50f6cc16580e0ad0bcc9429459
Miguel M Pacheco <miguelmpacheco8218@gmail.com>
2026-09-13 22:04:42 -0300

    teste de push pelo gitlens

--- codes/CursoemVideo/Desafios/des107/des107.py
+++ codes/CursoemVideo/Desafios/des107/des107.py
@@ -5,3 +5,4 @@ print(f'A metade de R${p:.2f} é R${moeda.metade(p):.2f}')
 print(f'O dobreo de R${p:.2f} é R${moeda.dobro(p):.2f}')
 print(f'Aumentando 10% de R${p:.2f}, temo R${moeda.aumentar(p, 10):.2f}')
 print(f'Reduzindo 13% de R${p:.2f}, temos R${moeda.diminuir(p, 13):.2f}')
+
